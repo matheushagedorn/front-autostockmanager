05d1d641e6efdf9b00c0dba1522d054059c6e78d
Arthur <tachiniarthur@gmail.com>
2024-05-21 21:00:18 -0300

    wip

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,17 +1,24 @@
 <template>
-    <div class="container-fluid">
-        <div class="container d-flex flex-column align-items-center justify-content-center">
-            <img src="/src/assets/img/car.png" style="width: 20rem; height: 20rem;" alt="Carro">
-            <h1>AutoStock Manager</h1>
-            
-            <div class="d-flex flex-column align-items-start justify-content-start gap-2 mb-4">
-                <label for="matricula">Matrícula do Usuário:</label>
+    <div class="container h-100 d-flex flex-column align-items-center justify-content-center bg-primary gap-4">
+        <div class="row">
+            <div class="col-12">
+                <img src="/src/assets/img/car.png" style="width: 20rem; height: 20rem;" alt="Carro">
+                <h1>AutoStock Manager</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 d-flex flex-column align-items-center justify-content-center gap-2">
+                <label class="fs-5" for="matricula">Matrícula do Usuário:</label>
                 <input type="email" id="matricula" placeholder="Matrícula do Usuário">
-                <label for="senha">Senha:</label>
+                <label class="fs-5" for="senha">Senha:</label>
                 <input type="password" id="senha" placeholder="••••••••">
             </div>
-            <router-link to="home"><button>Entrar</button></router-link>
-            <router-link to="register"><button style="margin-top: 0.75rem;">Registre-se</button></router-link>
+        </div>
+        <div class="row">
+            <div class="col-12 d-flex flex-column align-items-center justify-content-center gap-2">
+                <router-link to="home"><button>Entrar</button></router-link>
+                <router-link to="register"><button style="margin-top: 0.75rem;">Registre-se</button></router-link>
+            </div>
         </div>
     </div>
 </template>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,28 +1,34 @@
 <template>
-    <div class="login">
-        <div class="login-container">
-            <img src="/src/assets/img/car.png" alt="Carro">
+    <div class="container-fluid">
+        <div class="container d-flex flex-column align-items-center justify-content-center">
+            <img src="/src/assets/img/car.png" style="width: 20rem; height: 20rem;" alt="Carro">
             <h1>AutoStock Manager</h1>
 
-            <div class="login-container-form">
-                <label for="matricula">Matrícula do Usuário:</label>
-                <input type="text" id="matricula" placeholder="Matrícula" maxlength="5">
-                <label for="senha">Senha:</label>
-                <input type="password" id="senha" placeholder="••••••••" minlegth="8">
-                <label for="confirma-senha">Confirme sua senha:</label>
-                <input type="password" id="confirma-senha" placeholder="••••••••" minlegth="8">
-                <label for="nome-completo">Nome completo:</label>
-                <input type="text" id="nome-completo" placeholder="Nome completo">
-                <label for="email">Email:</label>
-                <input type="email" id="email" placeholder="exemplo@email.com.br">
-                <button @click="cadastrar">Cadastre-se</button>
-                <router-link to="/"><button style="margin-top: 0.75rem">Voltar para login</button></router-link>
+            <div class="row">
+                <div class="col-12 d-flex flex-column align-items-start justify-content-start gap-2 mb-4">
+                    <label for="matricula">Matrícula do Usuário:</label>
+                    <input type="text" id="matricula" placeholder="Matrícula" maxlength="5">
+                    <label for="senha">Senha:</label>
+                    <input type="password" id="senha" placeholder="••••••••" minlegth="8">
+                    <label for="confirma-senha">Confirme sua senha:</label>
+                    <input type="password" id="confirma-senha" placeholder="••••••••" minlegth="8">
+                    <label for="nome-completo">Nome completo:</label>
+                    <input type="text" id="nome-completo" placeholder="Nome completo">
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" placeholder="exemplo@email.com.br">
+                </div>
             </div>
+            <button @click="cadastrar">Cadastre-se</button>
+            <router-link to="/"><button style="margin-top: 0.75rem">Voltar para login</button></router-link>
         </div>
     </div>
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
 async function cadastrar() {
     const matricula = document.getElementById('matricula').value;
     const senha = document.getElementById('senha').value;
@@ -35,18 +41,16 @@ async function cadastrar() {
         return;
     }
 
-    const response = await fetch('localhost:8080/cadastrar-usuario', { // atualizar para link da api
+    const response = await fetch('http://localhost:8080/usuarios', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            modelo: 'Fusca',
-            marca: 'Volkswagen',
-            cor: 'Preto',
-            ano: 1970,
-            quilometragem: 100000,
-            valor_revenda: 10000,
+            matricula: matricula,
+            senha: senha,
+            nome: nomeCompleto,
+            email: email,
         }),
     });
 
